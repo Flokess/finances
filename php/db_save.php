d0d627ce45955d274a510ca2fbc3b32168e6a0bb
egorr <egorrka0406@gmail.com>
2022-11-08 18:21:03 +0300

    added result.php

--- php/db_save.php
+++ php/db_save.php
@@ -1,1 +1,18 @@
 <?php
+include $_SERVER['DOCUMENT_ROOT']."/vendor/autoload.php";
+
+class_alias('\RedBeanPHP\R', '\R');
+R::setup('mysql:host=127.0.0.1;dbname=expenses', 'root', 'root');
+
+$entry = $_POST['entry'];
+
+
+if(!R::testConnection())
+{
+    exit("Нет подключения к бд");
+}
+
+$table = R::dispense('money');
+$table -> sum = $entry;
+
+R::store($table);
---
+++ php/result.php
@@ -0,0 +1,5 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT']."/vendor/autoload.php";
+
+class_alias('\RedBeanPHP\R', '\R');
+R::setup('mysql:host=127.0.0.1;dbname=expenses', 'root', 'root');
