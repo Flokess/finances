91d3c67cfa4a8f14095c9b8ae7b955673918f4e3
egorr <egorrka0406@gmail.com>
2022-11-08 14:57:29 +0300

    added style.css

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
-<?php
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>expense calculator</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<div class="container mt-4">
+    <h1>Система по учёту доходов и расходов</h1>
+    <form action="" method="post">
+        <input type="text" class="form-control" name="сумма" id="sum" placeholder="Введите сумму"><br>
+        <button class="btn btn-success">Записать</button>
+    </form>
 
 
 
-require_once __DIR__ . '/vendor/autoload.php';
+</div>
+</body>
+</html>
+
+
