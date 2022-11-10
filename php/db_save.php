7bf095fbfd665bae0f9ea9260583e1d64a1a8412
egorr <egorrka0406@gmail.com>
2022-11-10 15:52:06 +0300

    final version

--- php/db_save.php
+++ php/db_save.php
@@ -5,20 +5,21 @@ class_alias('\RedBeanPHP\R', '\R');
 R::setup('mysql:host=127.0.0.1;dbname=expenses', 'root', 'root');
 
 $entry = $_POST['entry'];
-
+$date = $_POST['date'];
 
 if(!R::testConnection())
 {
     exit("Нет подключения к бд");
 }
 
-if ($entry == 0)
+if ($entry == 0 | $date == 0)
 {
-    ?><script> alert("ВВЕДИТЕ СУММУ");</script>    <?php
+    ?><script> alert("ВВЕДИТЕ СУММУ И ДАТУ");</script>  <?php
 }else
 {
     $table = R::dispense('money');
     $table -> sum = $entry;
+    $table -> date = $date;
 
     R::store($table);
     header('Location: /');
--- php/result.php
+++ php/result.php
@@ -1,6 +1,5 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT']."/vendor/autoload.php";
-
 class_alias('\RedBeanPHP\R', '\R');
 R::setup('mysql:host=127.0.0.1;dbname=expenses', 'root', 'root');
 
@@ -9,15 +8,59 @@ function dump($what)
 {
     echo '<pre>';print_r($what);echo '</pre>';
 }
-$expenses = R::findAll('money');
-$limit = 150;
-$expenses = R::findAll('money' , 'ORDER BY id ASC LIMIT ?', [$limit]);
 
-
-$sum = 0;
-foreach ($expenses as $expens){
-    $sasd = $expens['sum'];
-     $sum += (int)$expens['sum'];
+if($_POST['month'] == 1){
+    $name_month = "Январь";
+}else if($_POST['month'] == 2){
+    $name_month = "Февраль";
+}else if($_POST['month'] == 3){
+    $name_month = "Март";
+}else if($_POST['month'] == 4){
+    $name_month = "Апрель";
+}else if($_POST['month'] == 5){
+    $name_month = "Май";
+}else if($_POST['month'] == 6){
+    $name_month = "Июнь";
+}else if($_POST['month'] == 7){
+    $name_month = "Июль";
+}else if($_POST['month'] == 8){
+    $name_month = "Август";
+}else if($_POST['month'] == 9){
+    $name_month = "Сентябрь";
+}else if($_POST['month'] == 10){
+    $name_month = "Октябрь";
+}else if($_POST['month'] == 11){
+    $name_month = "Ноябрь";
+}else if($_POST['month'] == 12){
+    $name_month = "Декабрь";
 }
 
-echo $sum;
+
+
+$year = date('Y');
+$number = cal_days_in_month(CAL_GREGORIAN, $_POST['month'], $year); // 31
+$date1 = date("Y-{$_POST['month']}-01");
+$date2 = date("Y-{$_POST['month']}-{$number}");
+
+
+
+$expenses = R::getAll("SELECT * FROM money WHERE date BETWEEN '{$date1}' AND '{$date2}'");
+$sum = 0;
+foreach ($expenses as $expens)
+{
+    $sum += (int)$expens['sum'];
+}
+
+
+
+$expenses = R::findAll('money');
+$sumALL = 0;
+foreach ($expenses as $expens)
+{
+    $sumALL += (int)$expens['sum'];
+}
+
+
+
+require_once "../index.html";
+
